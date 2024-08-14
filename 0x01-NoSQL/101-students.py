a9535960ce2cc0be6773be933562671e2ab493e9
VeeSimz <kayveesimba@gmail.com>
2024-08-14 03:36:44 +0200

    Advanced tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Implementing use of pymongo in MongoDB Operations with Python """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    top_st = mongo_collection.aggregate([
+        {
+            "$project": {
+              "name": "$name",
+              "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+    return top_st
